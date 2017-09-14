b252ea825d1057bb264925f5487bb725bd7b50e1
Joyce <AJOYCE@corp.vha.ad>
2017-09-14 09:49:00 -0500

    Nothing it currently broken, but the authenticated user notes are not displaying

--- src/app/data-repo.service.ts
+++ src/app/data-repo.service.ts
@@ -7,18 +7,19 @@ import { async } from 'rxjs/scheduler/async';
 @Injectable()
 export class DataRepoService {
   userList: FirebaseListObservable<any[]>;
+  teamFeedbackNotes: FirebaseListObservable<any[]>;
+  userDetails: FirebaseListObservable<any[]>;
 
   constructor(
     public db: AngularFireDatabase,
     public authService: AuthenticationService
   ) {
-    this.userList = this.db.list('/users');
-  }
-
-  public async getTeamFeedback() {
-    const user = await this.authService.user;
-    user.subscribe(result => {
-      return this.db.list('/teamFeedbackNotes/' + result.uid);
+    authService.user.subscribe(user => {
+      this.userList = this.db.list('/users');
+      if (user) {
+        this.userDetails = this.db.list('/users/' + user.uid.toString());
+        this.teamFeedbackNotes = this.db.list('/teamFeedbackNotes/' + user.uid.toString());
+      }
     });
   }
 }
